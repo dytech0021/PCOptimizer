e2a1e1aaf69e863025acc7ea65e6a991f5766d3a
dytech0021 <dytech0021@users.noreply.github.com>
2026-05-27 17:55:44 -0300

    Corrige build: remove dependencia externa e ajusta target para .NET 10

--- PCOptimizer/Services/ServiceOptimizer.cs
+++ PCOptimizer/Services/ServiceOptimizer.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ServiceProcess;
-using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace PCOptimizer.Services
 {
@@ -28,27 +25,35 @@ namespace PCOptimizer.Services
 
             foreach (var serviceName in UnnecessaryServices)
             {
-                try
-                {
-                    using var sc = new ServiceController(serviceName);
-                    if (sc.Status == ServiceControllerStatus.Running)
-                    {
-                        sc.Stop();
-                        sc.WaitForStatus(ServiceControllerStatus.Stopped, TimeSpan.FromSeconds(10));
-                    }
-
-                    using var key = Registry.LocalMachine.OpenSubKey(
-                        $@"SYSTEM\CurrentControlSet\Services\{serviceName}", writable: true);
-                    if (key != null)
-                    {
-                        key.SetValue("Start", 4); // 4 = Disabled
-                        optimized++;
-                    }
-                }
-                catch { }
+                if (RunSc($"stop {serviceName}") && RunSc($"config {serviceName} start= disabled"))
+                    optimized++;
             }
 
             return optimized;
+        }
+
+        private static bool RunSc(string arguments)
+        {
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = "sc.exe",
+                    Arguments = arguments,
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                };
+
+                using var process = Process.Start(psi);
+                process?.WaitForExit(10000);
+                return process?.ExitCode == 0;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
